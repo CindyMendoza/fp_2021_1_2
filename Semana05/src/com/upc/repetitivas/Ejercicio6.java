c16a229545929c77acb9ebf277fcd6611aa14a42
Carlos Alberto <pciscflo@upc.edu.pe>
2021-06-19 10:45:41 -0500

    19.06.03

--- Semana05/src/com/upc/repetitivas/Ejercicio6.java
+++ Semana05/src/com/upc/repetitivas/Ejercicio6.java
@@ -6,11 +6,55 @@ public class Ejercicio6 {
   1) Que reciba un número positivo entero y retorne la suma de sus dígitos
   2) Que retorne la suma de los cuadrados los n primeros numero enteros desde 1.
      1^2 + 2^2 + 3^2 + .......n^2
-  3) Que retorne la suma de los cubos de los n primeros números múltiplos de 3.
+  3) Calcular la siguiente suma:
       3^3 + 6^3 + 9^3 ....+ n términos
   */
     public static void main(String[] args) {
         System.out.println(obtenerSumaDigitos(233));
+        System.out.println(sumaCuadrados(10));
+        System.out.println("Suma cubos:" + sumarCubos1(2));
+        System.out.println("Suma cubos:" + sumarCubos2(2));
+        System.out.println("Suma cubos:" + sumarCubos3(2));
+    }
+    public static double sumarCubos1(int cantidadTerminos){
+        double suma=0;
+        int contador=3;
+        for (int i = 1; i <= cantidadTerminos; i++) {
+            suma = suma + Math.pow(contador,3);// 3^3 + 6^3
+            contador = contador + 3;
+        }
+        return suma;
+    }
+
+    public static double sumarCubos2(int num){ //num cantidad de terminos
+        double suma=0;
+        for (int i = 1; i <= num; i++) {
+            suma = suma + Math.pow(i*3,3);//3^3 + 6^3
+        }
+        return suma;
+    }
+    public static double sumarCubos3(int cantidadTerminos){
+        double suma=0;
+        int base=1;
+        int contadorTerminos=0;
+        while (true){
+            base++;
+            if (base % 3 ==0){
+                suma+= Math.pow(base,3);
+                contadorTerminos++;
+            }
+            if (contadorTerminos==cantidadTerminos){
+                break;//sale del while
+            }
+        }
+        return suma;
+    }
+    private static int sumaCuadrados(int n){
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            suma+=Math.pow(i,2);
+        }
+        return suma;
     }
     private static int obtenerSumaDigitos(int numero){
         int digito, suma = 0;
