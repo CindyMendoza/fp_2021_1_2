8ae50cf01929bc54b4ccdd5fcaf4274937b67ad4
Carlos Alberto <pciscflo@upc.edu.pe>
2021-06-19 08:26:38 -0500

    19.06.01

---
+++ Semana05/src/com/upc/repetitivas/Ejercicio1.java
@@ -0,0 +1,9 @@
+package com.upc.repetitivas;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){ // el primero sólo se ejecuta la primera vez, luego el incremento y condición se evalúa cada vezq eue se repite
+            System.out.println(i);//0,1,2,....9
+        }
+    }
+}
