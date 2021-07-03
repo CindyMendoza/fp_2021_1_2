5588a5199fd044db7bb1ff7582e30cbaf126cae6
Carlos Alberto <pciscflo@upc.edu.pe>
2021-07-03 10:51:15 -0500

    03.07.06

---
+++ Semana07/src/com/upc/cadenas/Ejercicio8.java
@@ -0,0 +1,26 @@
+package com.upc.cadenas;
+
+public class Ejercicio8 {
+    static String[] codigos = {};
+    public static void main(String[] args) {
+        System.out.println(obtenerCantidadFiguras("MO00100614520170013","jugadores"));
+    }
+    private static int obtenerCantidadFiguras(String codigo, String tipo){ //no usa for
+
+        return 0;
+    }
+
+    private  static int obtenerCantidadAlbumes(String sede){ //susas for para codigos
+
+        return 0;
+    }
+
+    private  static String obtenerCodigoAlumnoConMasFiguras(){
+
+        return "";
+    }
+    private  static String obtenerCantidadAlbumsMenos250Figuras(){
+
+        return "";
+    }
+}
